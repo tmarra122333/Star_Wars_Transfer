a477b2ac4aea5d030e8332fa7de392a85bd7cf7c
Tmarra122333 <Tmarra122333@gmail.com>
2021-12-15 20:36:29 -0500

    Act 1 Homework Complete

--- act2.sh
+++ act2.sh
@@ -1,5 +1,5 @@
 # 1. Inside the `star_wars/rebellion` directory, (IN ONE COMMAND) create a file called princess_leia.txt with the text "Help me, Obi-Wan…You’re my only hope."
-
+touch princess_leia.txt && echo "Help me, Obi-Wan...You're my only hope." > ~/princess_leia.txt
 # 2. Create a file called obi_wan.txt in star_wars/rebellion
 
 # 3. Create a file in star_wars/rebellion called luke_skywalker.txt
